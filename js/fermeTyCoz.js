45b036c7739898e6b7f5aea3b5a4580eedd2e901
Ophélie HORNN <hornn.ophelie@gmail.com>
2021-04-21 11:46:05 +0200

    ajout de la page producteur

---
+++ js/fermeTyCoz.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  direction: "horizontal",
+  spaceBetween: 100,
+  speed: 400,
+  keyboard: {
+    enabled: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
