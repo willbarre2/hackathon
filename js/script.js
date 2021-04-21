d01e504994ae6f0169d3470971ff5d457a930819
Ophélie HORNN <hornn.ophelie@gmail.com>
2021-04-22 00:07:32 +0200

    ajout slider, logo et newsletters

--- js/script.js
+++ js/script.js
@@ -29,3 +29,83 @@ $(document).ready(function () {
     }
   });
 });
+
+// Slider Recettes
+// =========================
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: 3,
+  spaceBetween: 30,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
+// Validation email
+// =========================
+("strict mode");
+const submitBtn = document.querySelector(".submit-btn");
+const emailInput = document.querySelector(".newsletters-email");
+const errorMessage = document.querySelector(".error-message");
+const modal = document.querySelector(".newsletters-modal-container");
+const closeModalBtn = document.querySelector(".close-modal");
+console.log(modal);
+
+function closeModal() {
+  modal.classList.add("hidden");
+}
+function showModal() {
+  modal.classList.remove("hidden");
+}
+
+function ValidateEmail(mail) {
+  if (
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+      mail
+    )
+  ) {
+    return true;
+  }
+  return false;
+}
+
+submitBtn.addEventListener("click", function (e) {
+  // Reset default values
+  e.preventDefault();
+  errorMessage.classList.add("hidden");
+  // Valid Email  -> Display Modal
+  if (ValidateEmail(emailInput.value)) {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    showModal();
+    emailInput.value = "";
+    emailInput.blur();
+  } else {
+    // email invalide: apparition du messagd d'erreur
+    // Set time ouf pour si l'utilsateur a rentré une mauvaise adresse alors que le message d'erreur existe deja
+    setTimeout(function () {
+      errorMessage.classList.remove("hidden");
+    }, 100);
+  }
+});
+
+//Ferme le modal : clic, bouton X ou touche Echap
+closeModalBtn.addEventListener("click", closeModal);
+modal.addEventListener("click", closeModal);
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") closeModal();
+});
