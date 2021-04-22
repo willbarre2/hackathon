1980f2dc53e4b6f62453e5e3974595a02bf9c292
William Barre <william.barre@campusdulac.com>
2021-04-22 09:16:33 +0200

    fetch master +remove carrotte

--- js/animScroll.js
+++
@@ -1,6 +0,0 @@
-const offsetInput = document.querySelector("#circle");
-
-window.addEventListener("scroll", (e) => {
-  let offset = `${window.scrollY / 42}%`;
-  document.documentElement.style.setProperty("--offset", offset);
-});
