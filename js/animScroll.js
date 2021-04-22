e73abf85678022e5e6f5fbf90baa47ebf05c7732
William Barre <william.barre@campusdulac.com>
2021-04-22 15:44:19 +0200

    modif bullets prod et js carotte

--- js/animScroll.js
+++ js/animScroll.js
@@ -1,6 +1,6 @@
-const offsetInput = document.querySelector("#circle");
+const offsetInput = document.getElementById("circle");
 
-window.addEventListener("scroll", (e) => {
+window.addEventListener("scroll", function (e) {
   let offset = `${window.scrollY / 78}%`;
   document.documentElement.style.setProperty("--offset", offset);
 });
